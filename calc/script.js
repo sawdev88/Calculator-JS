8a9196b0b96eda0868250d5b9183b541e04e03be
Sean Wines <seanwinesdev@gmail.com>
2016-07-28 17:10:28 -0700

    added comments to JS

--- calc/script.js
+++ calc/script.js
@@ -1,22 +1,30 @@
 var
   input = document.getElementById('inputField'),
   buttons = document.querySelectorAll('button'),
-  arr = [];
+  values = [];
 
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
 
     if (this.value === '=') {
-      var equals = arr.join('');
+      // Turn values into string and evaluate
+      var equals = values.join('');
       var answer = eval(equals);
+
+      // Display answer
       input.innerHTML = eval(answer);
-      arr = [];
-      arr.push(answer);
+
+      // Clear array to prevent OOO and store answer as first param
+      values = [];
+      values.push(answer);
+
     } else if (this.value === 'clear') {
-      arr = [];
+      // Clear all info
+      values = [];
       input.innerHTML = '';
     }else {
-      arr.push(this.value);
+      // Add values to array and display
+      values.push(this.value);
       input.innerHTML += this.value;
     }
 
