336a93ac810fefcd0f562077a26a1d6759efb066
Sean Wines <seanwinesdev@gmail.com>
2016-08-09 16:43:19 -0700

    Refactored js

--- calc/script.js
+++ calc/script.js
@@ -1,49 +1,34 @@
-var
-  display = document.getElementById('displayField'),
-  buttons = document.querySelectorAll('button'),
-  values = [],
-  colors = ['#FF5A5A', '#32ADFF', '#ad5aff'];
-
-function randomColor() {
-  var randomNumber = Math.floor(Math.random() * colors.length);
-  var randColor = colors[randomNumber];
-
-  document.body.style.background = randColor;
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].style.color = randColor;
-  }
-}
+var display = document.getElementById('displayField'),
+    buttons = document.querySelectorAll('button'),
+    values = [];
 
 function clearArray(arr) {
-  arr.splice(0);
+    arr.splice(0);
 }
 
-randomColor();
-
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
+    buttons[i].addEventListener('click', function() {
+        if (this.value === '=') {
+            // Turn values into string and evaluate
+            var equals = values.join('');
+            var answer = eval(equals);
+            // Display answer
+            display.innerHTML = answer;
+            // Clear array to prevent OOO and store answer as first param
+            clearArray(values);
+            values.push(answer);
 
-    if (this.value === '=') {
-      // Turn values into string and evaluate
-      var equals = values.join('');
-      var answer = eval(equals);
+        } else if (this.value === 'clear') {
+            // Clear all info
+            clearArray(values);
+            display.innerHTML = '';
 
-      // Display answer
-      display.innerHTML = eval(answer);
+        } else {
+            // Add values to array and display
+            values.push(this.value);
+            display.innerHTML += this.value;
 
-      // Clear array to prevent OOO and store answer as first param
-      clearArray(values);
-      values.push(answer);
+        }
 
-    } else if (this.value === 'clear') {
-      // Clear all info
-      clearArray(values);
-      display.innerHTML = '';
-    }else {
-      // Add values to array and display
-      values.push(this.value);
-      display.innerHTML += this.value;
-    }
-
-  })
+    })
 }
